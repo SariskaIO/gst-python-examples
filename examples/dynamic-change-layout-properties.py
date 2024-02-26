3a9843b701d9f20548b714bee87ccbb849fe38e5
dipak140 <dipak@sariska.io>
2024-02-26 17:30:49 +0530

    Adjust sink pad size when participant leaves

--- examples/dynamic-change-layout-properties.py
+++ examples/dynamic-change-layout-properties.py
@@ -14,11 +14,12 @@ log = logging.getLogger("main")
 
 def add_video_sources(pipe, loop):
     log.info("Adding videotestsrcs")
+    # Create Sources
     srcs = [Gst.ElementFactory.make("videotestsrc") for _ in range(4)]
 
     sink = Gst.ElementFactory.make("autovideosink", "autovideosink")
-    pipe.add(sink)
 
+    pipe.add(sink)
     log.info("Adding compositor")
     compositor = Gst.ElementFactory.make("compositor", "compositor")
     # Add elements to the pipeline
@@ -33,7 +34,6 @@ def add_video_sources(pipe, loop):
     [pads[i].set_property('ypos', y) for i, (_, y) in enumerate(pad_properties)]
     [pad.set_property('width', 320) for pad in pads]
     [pad.set_property('height', 320) for pad in pads]
-
 
    # Get the source pads
     for i, src in enumerate(srcs):
@@ -50,12 +50,32 @@ def add_video_sources(pipe, loop):
 
     log.info("Waiting for a while before removing one source")
     GLib.timeout_add_seconds(5, remove_video_source, pipe, srcs[3], pads[3], compositor)
-    GLib.timeout_add_seconds(10, remove_video_source, pipe, srcs[2], pads[2], compositor)
 
 def remove_video_source(pipe, src, pad2, compositor):   
     log.debug(src.set_state(Gst.State.NULL))  # (5)
     log.debug(pipe.remove(src))
     log.debug(compositor.release_request_pad(pad2))  # (6)
+    sink_pad = compositor.get_static_pad("sink_2")
+    sink_pad.set_property('xpos', 160)
+    sink_pad.set_property('ypos', 320)
+
+    
+
+def add_new_video_source(pipe, compositor):
+    pipe.set_state(Gst.State.PAUSED)
+    src = Gst.ElementFactory.make("videotestsrc")
+    pipe.add(src)
+    srcpad = src.get_static_pad("src")
+    sink_pad = compositor.get_request_pad(f'sink_2')
+    sink_pad.set_property('xpos', 0)
+    sink_pad.set_property('ypos', 320)
+    sink_pad.set_property('width', 320)
+    sink_pad.set_property('height', 320)
+    srcpad.link(sink_pad)
+    src.link(compositor)
+    pipe.set_state(Gst.State.PLAYING)
+    print("Nothing")
+
 
 def main(args):
     GObject.threads_init()
